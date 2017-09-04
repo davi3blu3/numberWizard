dccf79020a48e53abea090ea0229743770c9e390
Dave Harned <dharned@gmail.com>
2017-09-04 17:01:13 -0500

    clean up, comment, and fix edge cases

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
-
-	int max = 1000;
-	int min = 1;
-	int guess = 500;
+	// initialize variables where they'll be available to multiple methods
+	int max;
+	int min;
+	int guess;
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +13,20 @@ public class NumberWizard : MonoBehaviour {
 	}
 	
 	void StartGame () {
+		 set variables at beginning of each game
+		max = 1000;
+		min = 1;
+		guess = 500;
+		
+		print ("=========================");
 		print ("Welcome to Number Wizard!");
 		print ("Pick a number in your head, but don't tell me.");
 		
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
+		
+		// alter max to allow choice of 1 or 1000
+		max = max + 1;
 		
 		print ("Is the number higher or lower than " + guess + "?");
 		print ("Please press: up arrow for higher, down arrow for lower, return for equal.");		
@@ -35,6 +44,7 @@ public class NumberWizard : MonoBehaviour {
 			NextGuess ();
 		} else if (Input.GetKeyDown("return"))	{
 			print ("I won!");
+			StartGame ();
 		}				
 	}
 	
