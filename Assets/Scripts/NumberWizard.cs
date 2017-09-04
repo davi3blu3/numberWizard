571fb092c200ebb2687455ac5d7e3658182c8d74
Dave Harned <dharned@gmail.com>
2017-09-04 13:47:12 -0500

    first commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Number Wizard!");
+		print ("Pick a number in your head, but don't tell me.");
+		
+		int max = 1000;
+		int min = 1;
+		
+		print ("The highest number you can pick is " + max);
+		print ("The lowest number you can pick is " + min);
+		
+		print ("Is the number higher or lower than 500?");
+		print ("Please press: up arrow for higher, down arrow for lower, return for equal.");
+		// char input = 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
